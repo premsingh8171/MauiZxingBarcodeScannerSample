ad35c1e77d73d792f45fad7287c9c4e486508869
premsingh8171 <premsingh8171@gmail.com>
2024-01-27 00:46:12 +0530

    Add project files.

---
+++ MauiZxingBarcodeScannerSample/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ZXing.Net.Maui;
+
+namespace MauiZxingBarcodeScannerSample;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	private void CameraBarcodeReaderView_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
+	{
+		Dispatcher.Dispatch(() =>
+		{
+			barcodeResult.Text = $"{e.Results[0].Value} {e.Results[0].Format}";
+		});
+    }
+}
+
